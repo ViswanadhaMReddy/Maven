162e6b723b94695cccc4e524d34892bffd2cec6a
Mr V <Mr V@DESKTOP-QVS0VCR.home>
2019-03-18 20:40:16 +0200

    6th Maven Commit

---
+++ Zfinal/src/test/java/comlogin/q/a/loginpage.java
@@ -0,0 +1,13 @@
+package comlogin.q.a;
+
+import org.testng.annotations.Test;
+
+public class loginpage {
+	
+	@Test
+	public void loginTest()
+	{
+		System.out.println("Welcome to Jenkins");
+	}
+
+}
